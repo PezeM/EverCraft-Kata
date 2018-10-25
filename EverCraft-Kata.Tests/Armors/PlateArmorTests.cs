263feceba75f26a62c70bae356cd5aa78e6bf150
PezeM <pezemos@gmail.com>
2018-10-25 12:59:11 +0200

    Iteration 4 - Added Plate Armor tests

---
+++ EverCraft-Kata.Tests/Armors/PlateArmorTests.cs
@@ -0,0 +1,51 @@
+﻿using EverCraft_Kata.Character;
+using EverCraft_Kata.Character.Classes;
+using EverCraft_Kata.Character.Races;
+using EverCraft_Kata.Equipment.Armors;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EverCraft_Kata.Tests.Armors
+{
+    [TestClass]
+    public class PlateArmorTests
+    {
+        private CharacterBaseModel character;
+        private CharacterBaseModel enemy;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            character = new CharacterBaseModel("Jake");
+            enemy = new CharacterBaseModel("Enemy");
+            enemy.ChangeRace(new Dwarf());
+            enemy.ChangeArmor(ArmorBase.PlateArmor);
+        }
+
+        [TestMethod]
+        public void PlateArmorIncreaseArmorClassBy8()
+        {
+            Assert.IsFalse(character.Attack(enemy, 17));
+        }
+
+        [TestMethod]
+        public void PlateArmorCanBeWornOnlyByFighterOrDwarf()
+        {
+            character.ChangeRace(new Dwarf());
+            character.ChangeArmor(ArmorBase.PlateArmor);
+            Assert.AreEqual(ArmorBase.PlateArmor, character.Armor);
+
+            var newChar = new Fighter("Jake");
+            newChar.ChangeArmor(ArmorBase.PlateArmor);
+            Assert.AreEqual(ArmorBase.PlateArmor, newChar.Armor);
+
+            var thirdChar = new Fighter("Elf");
+            thirdChar.ChangeRace(new Elf());
+            thirdChar.ChangeArmor(ArmorBase.PlateArmor);
+            Assert.AreEqual(ArmorBase.PlateArmor, thirdChar.Armor);
+
+            var fourthChar = new Rogue("Rogue");
+            fourthChar.ChangeArmor(ArmorBase.PlateArmor);
+            Assert.AreNotEqual(ArmorBase.PlateArmor, fourthChar.Armor);
+        }
+    }
+}
